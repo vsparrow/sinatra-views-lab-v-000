5dcd6f52ce367778dc2e52c986e75e3f909e0bd9
vsparrow <khan@vsparrow.com>
2018-03-03 02:42:54 -0500

    setup erb files hello, goodby, and date

--- app.rb
+++ app.rb
@@ -2,7 +2,16 @@ class App < Sinatra::Base
 
 	get '/' do
 		erb :index
+		# resp.status = 200
 	end
-
+	get '/hello' do
+		erb :hello
+	end
+	get '/goodbye' do
+		erb :goodbye
+	end
+	get '/date' do
+		erb :date
+	end
 
 end
